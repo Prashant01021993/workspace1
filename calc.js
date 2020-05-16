c8547dadea3e1755e8f93d49b9a44d96bbed2cda
Your Name <you@example.com>
2020-05-17 03:56:04 +0530

    Final project

---
+++ calc.js
@@ -0,0 +1,27 @@
+var numfield1 = document.getElementById('numfield1');
+var numfield2 =document.getElementById('numfield2');
+var resultID =document.getElementById('resultID');
+var form =document.getElementById('XY');
+
+
+form.addEventListener('submit',function (event){
+    if (!numfield1.value || !numfield2.value) {
+        alert("please enter the value");
+    } else {
+        
+        var x =parseFloat(numfield1.value);
+       
+        var y =parseFloat(numfield2.value);
+        var result = x/y;
+        var percent = result * 100;
+        resultID.innerText = "answer :" + percent + "%";
+        event.preventDefault();
+    }
+    
+}
+);
+
+
+/*resultID.innertext= resultID;*/
+
+
